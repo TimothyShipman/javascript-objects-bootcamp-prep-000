39ba8393f03e498799fbdab58a73e66d7e704f12
Tim Shipman <bricklayertim@yahoo.com>
2017-11-01 07:57:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {artistName: "songTitle"};
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({},playlist,{ 'Phil Ochs': "Here's to the State of Mississippi" })
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye
+}
